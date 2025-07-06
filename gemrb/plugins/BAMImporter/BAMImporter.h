5053f41c6f11cad10df648af6ac4f30623d4ed41
Ryein Goddard <ryein@goddardlabs.com>
2025-07-05 18:12:11 -0700

    made some getters and some access public

--- gemrb/plugins/BAMImporter/BAMImporter.h
+++ gemrb/plugins/BAMImporter/BAMImporter.h
@@ -27,72 +27,71 @@
 namespace GemRB {
 
 struct FrameEntry {
-	Region bounds;
-	bool RLE = false;
-	union {
-		strpos_t dataOffset = 0;
-		struct {
-			uint16_t dataBlockIdx;
-			uint16_t dataBlockCount;
-		} v2;
-	} location;
+  Region bounds;
+  bool RLE = false;
+  union {
+    strpos_t dataOffset = 0;
+    struct {
+      uint16_t dataBlockIdx;
+      uint16_t dataBlockCount;
+    } v2;
+  } location;
 };
 
 class ImageMgr;
 class Palette;
 
 enum class BAMVersion {
-	V1,
-	V2
+  V1,
+  V2
 };
 
 struct BAMV2DataBlock {
-	ieDword pvrzPage;
-	Point source;
-	Size size;
-	Point destination;
+  ieDword pvrzPage;
+  Point source;
+  Size size;
+  Point destination;
 };
 
 class BAMImporter : public AnimationMgr {
 public:
-	bool Import(DataStream* stream) override;
-	index_t GetCycleSize(index_t Cycle) override;
-	std::shared_ptr<AnimationFactory> GetAnimationFactory(const ResRef& resref, bool allowCompression = true) override;
-	/** Debug Function: Returns the Global Animation Palette as a Sprite2D Object.
-	If the Global Animation Palette is NULL, returns NULL. */
-	Holder<Sprite2D> GetPalette() override;
-	index_t GetCycleCount() override
-	{
-		return cycles.size();
-	}
+  bool Import(DataStream* stream) override;
+  index_t GetCycleSize(index_t Cycle) override;
+  std::shared_ptr<AnimationFactory> GetAnimationFactory(const ResRef& resref, bool allowCompression = true) override;
+  /** Debug Function: Returns the Global Animation Palette as a Sprite2D Object.
+  If the Global Animation Palette is NULL, returns NULL. */
+  Holder<Sprite2D> GetPalette() override;
+  index_t GetCycleCount() override
+  {
+    return cycles.size();
+  }
 
-	// Custom methods for Project-IE-4k
-	const Palette* GetPalettePtr() const { return palette.get(); }
-	//const std::vector<FrameEntry>& GetFrames() const { return frames; }
-	//const std::vector<CycleEntry>& GetCycles() const { return cycles; }
-	//Holder<Sprite2D> GetV2FramePublic(const FrameEntry& frame) { return GetV2Frame(frame); }
+  // moved for Project-IE-4k
+  using CycleEntry = AnimationFactory::CycleEntry;
+  // Custom methods for Project-IE-4k
+  const Palette* GetPalettePtr() const { return palette.get(); }
+  const std::vector<FrameEntry>& getFrames() const { return frames; }
+  const std::vector<CycleEntry>& getCycles() const { return cycles; }
+  ieByte GetCompressedColorIndex() const { return CompressedColorIndex; }
 
 private:
-	using CycleEntry = AnimationFactory::CycleEntry;
+  BAMVersion version = BAMVersion::V1;
+  std::vector<FrameEntry> frames;
+  std::vector<CycleEntry> cycles;
+  Holder<Palette> palette;
+  ieByte CompressedColorIndex = 0;
+  ieDword FLTOffset = 0;
+  ieDword CyclesOffset = 0;
+  ieDword FramesOffset = 0;
+  strpos_t DataStart = 0;
+  ResourceHolder<ImageMgr> lastPVRZ;
+  ieDword lastPVRZPage = 0;
 
-	BAMVersion version = BAMVersion::V1;
-	std::vector<FrameEntry> frames;
-	std::vector<CycleEntry> cycles;
-	Holder<Palette> palette;
-	ieByte CompressedColorIndex = 0;
-	ieDword FLTOffset = 0;
-	ieDword CyclesOffset = 0;
-	ieDword FramesOffset = 0;
-	strpos_t DataStart = 0;
-	ResourceHolder<ImageMgr> lastPVRZ;
-	ieDword lastPVRZPage = 0;
-
-	void Blit(const FrameEntry& frame, const BAMV2DataBlock& dataBlock, uint8_t* data);
-	std::vector<index_t> CacheFLT();
-	Holder<Sprite2D> GetV2Frame(const FrameEntry& frame);
-	Holder<Sprite2D> GetFrameInternal(const FrameEntry& frame, bool RLESprite, uint8_t* data) const;
+  void Blit(const FrameEntry& frame, const BAMV2DataBlock& dataBlock, uint8_t* data);
+  std::vector<index_t> CacheFLT();
+  Holder<Sprite2D> GetV2Frame(const FrameEntry& frame);
+  Holder<Sprite2D> GetFrameInternal(const FrameEntry& frame, bool RLESprite, uint8_t* data) const;
 };
 
 }
-
 #endif
